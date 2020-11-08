1f3581fb863fdebf12368986fde8aa18b913f9c8
Detlef Groth <dgroth@uni-potsdam.de>
2020-11-08 09:22:23 +0100

    chesschart added

---
+++ lib/chesschart/pkgIndex.tcl
@@ -0,0 +1,31 @@
+##############################################################################
+#
+#  System        : 
+#  Module        : 
+#  Object Name   : $RCSfile$
+#  Revision      : $Revision$
+#  Date          : $Date$
+#  Author        : $Author$
+#  Created By    : Dr. Detlef Groth
+#  Created       : Sun Nov 8 09:16:13 2020
+#  Last Modified : <201108.0916>
+#
+#  Description	
+#
+#  Notes
+#
+#  History
+#	
+##############################################################################
+#
+#  Copyright (c) 2020 Dr. Detlef Groth.
+# 
+#  All Rights Reserved.
+# 
+#  This  document  may  not, in  whole  or in  part, be  copied,  photocopied,
+#  reproduced,  translated,  or  reduced to any  electronic  medium or machine
+#  readable form without prior written consent from Dr. Detlef Groth.
+#
+##############################################################################
+
+
