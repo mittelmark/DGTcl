e8517bb895415891c82cc12a9008c02ca99cf4d0
Detlef Groth <dgroth@uni-potsdam.de>
2021-09-03 08:20:33 +0200

    dotcanvas now with local variables only and file-save as svg, png, pdf, jpeg

--- bin/dotcanvas.tcl
+++ bin/dotcanvas.tcl
@@ -3,7 +3,7 @@
 #
 #  Created By    : Dr. Detlef Groth
 #  Created       : Thu Sep 2 04:27:43 2021
-#  Last Modified : <210902.0641>
+#  Last Modified : <210903.0818>
 #
 #  Description	
 #
@@ -19,14 +19,19 @@
 # 
 ##############################################################################
 package require Tk
-package provide dotcanvas 0.1
+package provide dotcanvas 0.2.0
 
 namespace eval dotcanvas {
     variable fname ""
+    variable einfo ""
+    variable _cwf
+    variable dotpath ""
+    variable w
     proc wcanvas {w file {mtime -}} {
+        variable _cwf
         set checkinterval 1000 ;# modify as needed
         if {$mtime eq "-"} {
-            if [info exists ::_cwf] {after cancel $::_cwf}
+            if [info exists _cwf] {after cancel $_cwf}
             set file [file join [pwd] $file]
             [namespace current]::wcanvas $w $file [file mtime $file]
         } else {
@@ -34,20 +39,12 @@ namespace eval dotcanvas {
             if {$newtime != $mtime} {
                 [namespace current]::canvasupdate $w $file
                 [namespace current]::wcanvas $w $file
-            } else {set ::_cwf [after $checkinterval [info level 0]]}
-        }
+            } else {set _cwf [after $checkinterval [info level 0]]}
+        } 
     }
-    proc canvasupdate {w file} {
-        variable fname 
-        puts update
-        if {$fname eq ""} {
-            $w delete all
-            return
-        }
-        set dot ""
-        if {$::tcl_platform(os) eq "Linux"} {
-            set dot dot
-        } else {
+    proc getDot {} {
+        variable dot
+        if {[lindex $::tcl_platform(platform) 0] eq "windows"} {
             if {[file exists "C:/Program Files/GraphViz/bin/dot.exe"]} {
                 set dot "C:/Program Files/GraphViz/bin/dot.exe"
             } elseif {[file exists "C:/Programme/Graphviz/bin/dot.exe"]} {
@@ -57,50 +54,126 @@ namespace eval dotcanvas {
                 tk_messageBox -title "Error!" -icon error -message "GraphViz application not found!\nInstall GraphViz!" -type ok
                 return
             }
+        } else {
+            if {[auto_execok dot] eq ""}  {
+                tk_messageBox -title "Error!" -icon error -message "GraphViz application not found!\nInstall GraphViz!" -type ok
+                set dot ""
+            } else {
+                set dot [auto_execok dot]
+            }
         }
-        if {[catch {
-             set res [exec $dot -Ttk $file]
-             #puts $res
-             set c $w
-             $c delete all
-             eval $res
-             set ::einfo "File [file tail $file] is ok! [clock format [file mtime $file]]"
-         }]} {
-                $w configure -background salmon
-                update
-                after 1000
-                set ::einfo [regsub {.+dot:(.+)\n +while.+} $::errorInfo "\\1"]
-                $w configure -background white
+    }
+    proc canvasupdate {path file} {
+        variable fname 
+        variable einfo
+        variable dot
+        variable w
+        set w $path
+        set fname $file
+        if {$fname eq ""} {
+            $w delete all
+            return
         }
+        [namespace current]::dot2file
     }
     proc dotcanvas {path {dotfile ""}} {
         variable fname
+        variable dot 
         set fname $dotfile
-        canvas $path -background white -width 200 -height 200 -borderwidth 10 -relief flat
+        canvas $path -background white -width 500 -height 400 -borderwidth 10 -relief flat
+        [namespace current]::getDot
+        if {$dot eq ""} {
+            $path create text 250 100 -text "Error: No GraphViz found!" -font "TkDefaultFont 20" -justify center -fill red
+            return $path
+        }
+        bind all <Control-c> [namespace current]::saveGraph
         if {$dotfile ne ""} {
             [namespace current]::wcanvas $path $dotfile
             [namespace current]::canvasupdate $path $dotfile
         }
         return $path
     }
+    proc dot2file {{outfile ""}} {
+        variable fname
+        variable w
+        variable dot
+        variable einfo
+        if {$outfile eq ""} {
+            set ext tk
+        } else {
+            set ext [string range [file extension $outfile] 1 end]
+        }
+        if {[catch {
+             set res [exec $dot -Ttk $fname]
+             set c $w
+             $c delete all
+             eval $res
+             set einfo "File [file tail $fname] is ok! [clock format [file mtime $fname]] - Use <Control-c> to save the Graph!"
+             if {$ext ne "tk"} {
+                 exec $dot -T$ext $fname -o $outfile
+                 set einfo "File [file tail $outfile] saved! [clock format [file mtime $outfile]]"
+             }
+         }]} {
+                $w configure -background salmon
+                update
+                after 1000
+                set einfo [regsub {.+dot:(.+)\n +while.+} $::errorInfo "\\1"]
+                $w configure -background white
+        }
+    }
+        
+    proc saveGraph {{filename ""}} {
+        variable fname
+        variable w
+        if {$filename eq ""} {
+            set types {
+                {{Png Files}       {.png}         }
+                {{Pdf Files}       {.pdf}         }                
+                {{Svg Files}       {.svg}         }
+                {{Jpeg Files}       {.jpg}         }
+                {{All Files}        *             }
+            }
+            set savefile [tk_getSaveFile -filetypes $types -initialdir [file dirname $fname] -initialfile [file rootname $fname].png]
+            if {$savefile != ""} {
+                set filename $savefile
+                
+            }
+        }
+        if {$filename eq ""} {
+            return
+        }
+        [namespace current]::dot2file $filename
+    }
+
 }
 if {[info exists argv0] && $argv0 eq [info script]} {
     if {[llength $argv] > 0} {
         if {[lindex $argv 0] eq "--help"} {
-            puts "dotcanvas dotfile.dot"
+            puts "tclsh dotcanvas.tcl dotfile.dot"
         } elseif  {[lindex $argv 0] eq "--version"} {
             puts "[package present dotcanvas]"
         } elseif {[file exists [lindex $argv 0]]} {
-            set ::einfo ""
             pack [dotcanvas::dotcanvas .c [lindex $argv 0]] -fill both -expand true  -padx 5 -pady 5 -ipadx 5 -ipady 5
-            pack [ttk::label .l -textvariable  ::einfo] -side top -fill x -expand false -padx 5 -pady 5
+            pack [ttk::label .l -textvariable  ::dotcanvas::einfo] -side top -fill x -expand false -padx 5 -pady 5
         } else {
             puts "Error: File [lindex $argv 0] does not exists!"
             exit 0
         }
     } else {
         puts "dotcanvas dotfile.dot"
+        exit 0
     }
 }
-        
-        
+
+# TODO:
+#   * make einfo namespace var (done)
+#   * tied text and canvas widget together
+#   * other layout engines, neato, fdp etc
+#   * widget docu
+#   * OSX port
+
+#' ## ChangeLog
+#' 
+#' * 2021-09-02 0.1 initial version         
+#' * 2021-09-02 0.2.0 all variables are now namespaces variables
+#' 
