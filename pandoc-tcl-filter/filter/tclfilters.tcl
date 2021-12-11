3efbb6dcc8343dec134fe257a26934a377d28a43
Detlef Groth <dgroth@uni-potsdam.de>
2021-12-11 19:35:44 +0100

    filter makefile added

--- pandoc-tcl-filter/filter/tclfilters.tcl
+++ pandoc-tcl-filter/filter/tclfilters.tcl
@@ -1,6 +1,6 @@
 package require Tcl
 
-package provide tclfilters 0.2.1
+package provide tclfilters 0.3.0
 
 source [file join [file dirname [info script]] filter-dot.tcl]
 source [file join [file dirname [info script]] filter-tsvg.tcl]
@@ -9,4 +9,6 @@ source [file join [file dirname [info script]] filter-pik.tcl]
 source [file join [file dirname [info script]] filter-pic.tcl]
 source [file join [file dirname [info script]] filter-eqn.tcl]
 source [file join [file dirname [info script]] filter-rplot.tcl]
+source [file join [file dirname [info script]] filter-mmd.tcl]
+source [file join [file dirname [info script]] filter-puml.tcl]
 
