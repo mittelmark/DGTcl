61d3d6d37745ae478641dd3e7e62ce9827084b3d
Detlef Groth <dgroth@uni-potsdam.de>
2022-02-05 20:22:56 +0100

    filter-pipe and filter-tcl files added

---
+++ pandoc-tcl-filter/filter/filter-pipe.tcl
@@ -0,0 +1,369 @@
+#' ---
+#' title: "filter-pipe.tcl documentation"
+#' author: "Detlef Groth, Caputh-Schwielowsee, Germany"
+#' date: 2022-01-12
+#' cmd:
+#'     results: show
+#' ---
+#' 
+#' ## Name
+#' 
+#' _filter-pipe.tcl_ - Filter which can be used to execute various programming
+#' languages using a pipe mechanism using the Tcl filter driver `pandoc-tcl-filter.tcl` and showing or hiding the output. 
+#' 
+#' ## Usage
+#' 
+#' The conversion of the Markdown documents via Pandoc should be done as follows:
+#' 
+#' ```
+#' pandoc input.md --filter pandoc-tcl-filter.tcl -s -o output.html
+#' ```
+#' 
+#' The file `filter-cmd.tcl` is not used directly but sourced automatically by the `pandoc-tcl-filter.tcl` file.
+#' If code blocks with the `.cmd` marker are found, the contents in the code block is processed via standard
+#' shell as command line application.
+#' 
+#' The following options can be given via code chunks or in the YAML header.
+#' 
+#' > - eval - should the code in the code block be evaluated, default: true
+#'   - echo - should the input code been shown, default: false
+#'   - pipe - the programming language to be used, either R or python, which is python3, default: python3
+#'   - results - should the output of the command line application been shown, should be asis, show or hide, default: show
+#' 
+#' To change the defaults the YAML header can be used. Here an example to change the 
+#' default pipe command to R.
+#' 
+#' ```
+#'  ----
+#'  title: "filter-pipe.tcl documentation"
+#'  author: "Detlef Groth, Caputh-Schwielowsee, Germany"
+#'  date: 2022-01-12
+#'  cmd:
+#'      pipe: R
+#'  ----
+#' ```
+#'
+#' ## Examples
+#' 
+#' ### Python examples
+#' 
+#' Here an example for very simple python code.
+#' 
+#' ```{.pipe pipe="python3" results="show" terminal=true}
+#' x=1
+#' print(x)
+#' y=x+1
+#' print(y)
+#' ```
+#' 
+#' In contrast to the [.cmd filter](filter-cmd.html) the code chunks are
+#' in the same session, so we can continue to add more Python code which knows about the existing variables from the session before.
+#' 
+#' Here an example declaring a function:
+#' 
+#' ```{.pipe pipe="python3" results="show" terminal=true}
+#' def test (x):
+#'     return(x+1)
+#' 
+#' print(test(y))
+#' ```
+#' 
+#' We can as well omit the terminal mode by setting the chunk option "terminal to false". You will then only see the output.
+#' 
+#' ```{.pipe terminal=false}
+#' def test2 (x):
+#'     return(x+2)
+#' 
+#' print(test2(y))
+#' ```
+#' 
+#' Let's finish the Python section with an image created with *matplotlib* (`{.pipe terminal=false results="hide"}`).
+#' 
+#' ```{.pipe terminal=false results="hide"}
+#' import matplotlib.pyplot as plt
+#'  
+#' # frequencies
+#' ages = [2,5,70,40,30,45,50,45,43,40,44,
+#'         60,7,13,57,18,90,77,32,21,20,40]
+#'  
+#' # setting the ranges and no. of intervals
+#' range = (0, 100)
+#' bins = 10
+#'  
+#' # plotting a histogram
+#' plt.hist(ages, bins, range, color = 'skyblue',
+#'         histtype = 'bar', rwidth = 0.8)
+#'  
+#' # x-axis label
+#' plt.xlabel('age')
+#' # frequency label
+#' plt.ylabel('No. of people')
+#' # plot title
+#' plt.title('My histogram')
+#' 
+#' # save the file
+#' plt.savefig('pyhist.png')
+#' ```
+#' 
+#' We then just display the image file using standard Markdown.
+#'  
+#' ![](pyhist.png)
+#' 
+#' There is as well the possibility to add inline Python code to display the values of variables or the output of short python commands here an example:
+#' 
+#' ```{.pipe pipe="python3"}
+#' l = [1,2,3,4]
+#' g = [1,2,3,4,5]
+#' print(len(l))
+#' ```
+#' 
+#' ```{.pipe pipe="python3"}
+#' g.append(6)
+#' print(len(g))
+#' ```
+#' 
+#' Let's now display the length of the list g, g has a length 
+#' of `py print(len(g));`!
+#' 
+#' Another trial: *py 3+2=* `py 3+2`? Should be five!
+#' 
+#' Let's finish with Python code containing an error:
+#' 
+#' ```{.pipe pipe="python3"}
+#' y=3
+#' # a type
+#' print(y00)
+#' ```
+#' 
+#' ### R examples
+#' 
+#' ```{.pipe pipe="R" results="show"}
+#' source("~/R/dlibs/chords.R")
+#' data(iris)
+#' png("testr.png")
+#' boxplot(iris$Sepal.Length ~ iris$Species,col=2:4)
+#' dev.off()
+#' print(2)
+#' 1
+#' ```
+#' 
+#' ![](testr.png)
+#' 
+#' Now a table example:
+#' 
+#' ```{.pipe pipe="R" results="asis"}
+#' data(mtcars)
+#' knitr::kable(head(mtcars[, 1:4]), "pipe")
+#' ```
+#' 
+#' There is as well the possibility to display inline R code. So for instance we can use the nrow function to 
+#' get the number of cars in the dataset.
+#' The mtchars dataset has `R nrow(mtcars)` cars!
+#' 
+#' Let's now simulate an R error:
+#' 
+#' ```{.pipe pipe="R"}
+#' x=3+2
+#' print(ls())
+#' x
+#' print(y)
+#' 2+3
+#' ```
+#' 
+#' ### Octave example
+#' 
+#' ```{.pipe pipe="oc"}  
+#' x=1;
+#' disp(x);
+#' y=2;
+#' disp(y);
+#' ```
+#' 
+#' Now let's do a plot:
+#' 
+#' ```{.pipe pipe="octave"}
+#' aux=figure('visible','off');
+#' tx = ty = linspace (-8, 8, 41);
+#' [xx, yy] = meshgrid (tx, ty);
+#' r = sqrt (xx .^ 2 + yy .^ 2) + eps;
+#' tz = sin (r) ./ r;
+#' mesh (tx, ty, tz);
+#' saveas(aux, 'mesh-octave.png', 'png');
+#' ```
+#' 
+#' ![](mesh-octave.png)
+#' 
+#' What about inline code? What is the value of y? 
+#' It should be `oc disp(y)`.  Hmm, not yet working.
+#' 
+#' ```{.pipe pipe="octave"}
+#' z=x+y
+#' disp(z)
+#' ```
+#' 
+#' Let's now do an Octave error:
+#' 
+#' ```{.pipe pipe="octave"}
+#' z
+#' disp(k)
+#' ```
+#' 
+#' ### Tcl example (not a pipe)
+#' 
+#' ```{.tcl}
+#' puts "Hello Tcl"
+#' set x 1
+#' ```
+#' 
+#' This is inline Tcl which shows the value of $x and x is: `tcl set x`!
+#'
+#' However until inside lists and in nested structure this is not yet evaluated like here in this list:
+#' 
+#' * a list item with Tcl code, x is: `tcl set x`
+#' * an other list item
+#' * a longer Tcl statement: `tcl set h "Hello World Number [incr x]!"`.
+#' * and here the current date and time: `tcl clock format [clock seconds] -format "%m %b, %Y"`
+#' 
+#' ## TODO:
+#' 
+#' * support Python (done) - inline (done) - error catching (done)
+#' * support R (done) - inline (done) - error catching (done)
+#' * support Octave (done) - inline (not working) - error catching (done)
+#' * support Julia??
+#' * terminal mode on/off
+#' 
+#' ## See also:
+#' 
+#' * [pandoc-tcl-filter Readme](../Readme.html)
+#' * [Rplot filter](filter-rplot.html)
+#' * [Tcl filter](../pandoc-tcl-filter.html)
+#' 
+
+namespace eval ::fpipe {
+    variable n 0
+    variable pipecode ""
+    variable pypipe ""
+    variable rpipe ""
+    variable opipe ""
+    #set n 0
+    #set pipecode ""
+}
+proc piperead {pipe args} {
+    if {![eof $pipe]} {
+        set got [gets $pipe]
+        if {$got ne "flush(stdout)"} {
+            # not sure why python does this
+            if {[regexp {^>>>} $got]} {
+                append ::fpipe::pipecode [regsub {^.*>>> } "$got" ""]
+            } else {
+                # R and Octave
+                append ::fpipe::pipecode "$got\n"
+            }
+            
+        }
+    } else {
+        close $pipe
+    }
+}
+
+
+# TODO: 
+# * namespace vars
+# * cleanup pipes if blocking true
+# * trace add execution exit enter YourCleanupProc
+
+proc filter-pipe {cont dict} {
+    incr ::fpipe::n
+    set ::fpipe::pipecode ""
+    set def [dict create results show eval true label null \
+             include true pipe python3 terminal true]
+    set dict [dict merge $def $dict]
+    if {$::fpipe::pypipe eq "" && [string range [dict get $dict pipe] 0 1] eq "py"} {
+        set ::fpipe::pypipe [open "|python3 -ui 2>@1" r+]
+        fconfigure $::fpipe::pypipe -buffering line -blocking false
+        fileevent $::fpipe::pypipe readable [list piperead $::fpipe::pypipe]
+    }
+    if {$::fpipe::opipe eq "" && [string range [dict get $dict pipe] 0 1] eq "oc"} {
+        set ::fpipe::opipe [open "|octave --interactive --no-gui --norc --silent 2>@1" r+]
+        fconfigure $::fpipe::opipe -buffering none -blocking false
+        fileevent $::fpipe::opipe readable [list piperead $::fpipe::opipe]
+        puts $::fpipe::opipe {PS1("")}
+        puts $::fpipe::opipe "page_screen_output(1);"
+        puts $::fpipe::opipe "page_output_immediately(1);"
+        puts $::fpipe::opipe "fflush(stdout)"
+        flush $::fpipe::opipe
+        after 100 [list append wait ""]
+        vwait wait
+        set ::fpipe::pipecode ""
+    }
+    #  
+    if {$::fpipe::rpipe eq "" && [string range [dict get $dict pipe] 0 0] eq "R"} {
+        set ::fpipe::rpipe [open "|R -q --interactive --vanilla  2>@1" r+]
+        fconfigure $::fpipe::rpipe -buffering line -blocking false
+        fileevent $::fpipe::rpipe readable [list piperead $::fpipe::rpipe]
+        
+    }
+    set wait [list]
+    set term ">>>"
+    if {[string range [dict get $dict pipe] 0 1] eq "oc"} {
+        set term "octave>"
+    }
+    if {[string range [dict get $dict pipe] 0 0] ne "X"} {
+        foreach line [split $cont \n] {
+            #  && [string range [dict get $dict pipe] 0 0] ne "R"
+            if {[dict get $dict terminal] && [string range [dict get $dict pipe] 0 1] in [list "py" "oc"]}  {
+                append ::fpipe::pipecode "$term $line\n" 
+                
+            }
+            if {[string range [dict get $dict pipe] 0 1] eq "py"} {
+                puts $::fpipe::pypipe "$line"
+            }
+            if {[string range [dict get $dict pipe] 0 1] eq "oc"} {
+                puts $::fpipe::opipe "$line"
+                flush $::fpipe::opipe
+            }
+            if {[string range [dict get $dict pipe] 0 0] eq "R"} {
+                puts $::fpipe::rpipe "$line"
+                flush $::fpipe::rpipe
+            }
+            if {[regexp {^[^\s]} $line]} {
+                # delay only if first letter is non-whitespace
+                # to allow to flush output
+                after 100 [list append wait ""]
+                vwait wait
+            }
+        }
+    }
+    after 100 [list append wait ""]
+    vwait wait
+    set res $::fpipe::pipecode
+    # TODO: dict app using
+    if {!([dict get $dict results] in [list show asis])} {
+        set res ""
+    } 
+    if {[dict get $dict results] eq "asis" && [string range [dict get $dict pipe] 0 0] eq "R"} {
+        set nres ""
+        foreach line [split $res \n] {
+            if {![regexp {^[+>]} $line]} {
+                append nres "$line\n"
+            }
+        }
+        set res $nres
+    }
+    if {[dict get $dict results] in [list show asis] && [string range [dict get $dict pipe] 0 1] eq "oc"} {
+        set nres ""
+        set i 0
+        foreach line [split $res \n] {
+            if {![regexp {^.*ans = 0\s*$} $line] && ![regexp {^>\s*$} $line]} {
+                append nres "$line\n"
+            } elseif {$i == 2  && [regexp {^\s*$} $line]} {
+                continue
+            } else {
+                set i 1
+            }
+            incr i
+        }
+        set res $nres
+    }
+    return [list $res ""]
+}
---
+++ pandoc-tcl-filter/filter/filter-tcl.tcl
@@ -0,0 +1,364 @@
+#' ---
+#' title: filter-tcl documentation
+#' author: Detlef Groth, Schwielowsee, Germany
+#' date: 2022-02-04
+#' ---
+#'
+#' ## NAME
+#' 
+#' _filter-tcl.tcl_ - filter to embed Tcl code in documentation.
+#' 
+#' The code for the Tcl filter is in pandoc-tcl-filter. It is to deeply involved into the main filter engine which drives the other Tcl and all other filters.
+#'
+#' ## CODE BLOCKS
+#' 
+#' Here an example:
+#' 
+#' ```
+#'     # indentation just to avoid 
+#'     # interpretation
+#'     ```{.tcl}
+#'     set x 1
+#'     puts $x
+#'     ```
+#' ```
+#' 
+#' Here the output:
+#' 
+#' ```{.tcl}
+#' set x 1
+#' puts $x
+#' ```
+#' 
+#' ### Code chunk options
+#' 
+#' The basic Tcl filter has three code chunk options:
+#' 
+#' - _eval_ - should the code be evaluated, default: true
+#' - _results_ - should the code output be shown, either "show", "hide" or "asis", default: "show"
+#' - _echo_ - should the code itself been shown, default: true
+#' 
+#' Here an example where we hide the code itself and only show the output using the chunk argument `echo=false`:
+#' 
+#' ```
+#'     ```{.tcl echo=false}
+#'     puts "The sinus of 3 is [expr {round(sin(3))}]"
+#'     ```
+#' ```
+#' 
+#' And here the output where the code is as well hidden:
+#' 
+#' ```{.tcl echo=false}
+#' puts "The sinus of 3 is [format %0.3f [expr {sin(3)}]]"
+#' ```
+#' 
+#' With the option eval we can just avoid interpretations of code chunks.
+#' 
+#' ```
+#'     ```{.tcl eval=false}
+#'     puts "The sinus of 3 is [format %0.3f [expr {sin(3)}]]"
+#'     ```
+#' ```
+#'
+#' And here the "output":
+#' 
+#' ```{.tcl eval=false}
+#' puts "The sinus of 3 is [format %0.3f [expr {sin(3)}]]"
+#' ```
+#'
+#' As we can use the *echo* chunk option to hide and show the code itself, we can as well do the same with the output. 
+#' So if we combine *echo="false"* and *results="hide"* the code will be evaluated but we will not seen anything in the output.
+#' This option is useful if you like to hide the Tcl code and just use it in the background for some computations. 
+#' 
+#' But what does the code chunk option *results="asis"* mean? 
+#' It allows you to create Markup code, such as Markdown if we you are writing
+#' in Markdown, using the Tcl programmiong language. 
+#' 
+#' ```
+#'     ```{.tcl results="asis"}
+#'     set md "This is **bold** and this is _italic_ text.\n"
+#'     append md "\nYou get the point?\n"
+#'     set md
+#'     ```
+#' ```
+#' 
+#' And here is the output:
+#' 
+#' ```{.tcl results="asis"}
+#' set md "This is **bold** and this is _italic_ text.\n"
+#' append md "\nYou get the point?\n"
+#' set md
+#' ```
+#' 
+#' Let's create a table based on a nested Tcl list, code chunk options - `{.tcl results="show"}`:
+#' 
+#' ```{.tcl results="show"}
+#' set l [list [list A B C D E] \
+#'    [list 1 2 3 4 5] \
+#'    [list 6 7 8 9 10] \
+#'    [list 11 12 13 14 15]]
+#' set md ""
+#' set x 0
+#' foreach row $l {
+#'     incr x
+#'     append md "|"
+#'     foreach cell $row {
+#'        append md " $cell |"
+#'     }
+#'     append md "\n"
+#'     if {$x == 1} {
+#'       append md "|"
+#'       foreach cell $row {
+#'           append md " ---- |"
+#'       }
+#'       append md "\n"
+#'     }
+#' }
+#' set md
+#' ```
+#' 
+#' If we now switch to `results="asis"` the output is this:
+#' 
+#' ```{.tcl results="asis"}
+#' # variable md still exists from the 
+#' # previous code chunk
+#' set md "<center>\n$md</center>\n"
+#' set md
+#' ```
+#' 
+#' As the display of tabular data is quite a common problem,
+#' the Tcl filter comes with a helper function *list2mdtab*, so we do not need to write this manual loop:
+#' 
+#' ```{.tcl results="asis"}
+#' # syntax: list2mdtab header rows
+#' # we need to flatten the row list
+#' set r <center>\n
+#' append r [list2mdtab [lindex $l 0] [concat {*}[lrange $l 1 end]]]
+#' append r \n</center>\n
+#' ```
+#' 
+
+#' ### Graphics
+#' 
+#' R has no default graphics engine for plotting etc. There is however the *tsvg* package which allows us to create SVG graphics. 
+#' As this package is included in the pandoc-tcl-filter application we can use it here and later include the generated graphis using the Markdown image markup:
+#' 
+#' ```{.tcl}
+#' package require tsvg
+#' tsvg set code ""
+#' tsvg circle -cx 50 -cy 50 -r 45 -stroke black \
+#'    -stroke-width 2 -fill green
+#' tsvg text -x 29 -y 45 Hello
+#' tsvg text -x 27 -y 65 World!
+#' tsvg write hw.svg
+#' ```
+#' 
+#' ![](hw.svg)
+#' 
+#' If [cairosvg](https://cairosvg.org/) is installed as well png files can be created. 
+#' Here an other example which creates a barplot and saves it as a PNG:
+#' 
+#' ```{.tcl}
+#' tsvg set code ""
+#' tsvg set width 400
+#' tsvg set height 400
+#' tsvg rect -x 10 -y 10 -width 380 -height 380 -fill #eeeee
+#' tsvg rect -x 20 -y 20 -width 360 -height 360 -fill #eeeee \
+#'    -style "stroke-width: 1px;stroke:black;"
+#' set col [list #ffaaaa #ffccaa #ffaacc #aaffff]
+#' set labels [list A B C D]
+#' foreach i [list  0 1 2 3] {
+#'     set val [expr {round(rand()*300)}]
+#'     tsvg rect -x [expr {($i+1)*70}] -y [expr {340-$val}] \
+#'           -width 50 -height $val \
+#'           -fill [lindex $col $i]
+#'     tsvg text -x [expr {($i+1)*70+20}] -y 370 [lindex $labels $i]
+#' }
+#' tsvg write barplot.png
+#' ```
+#' 
+#' ![](barplot.png)
+#' 
+#' PDF and PNG output might be prefered if your final document is a PDF document created via LaTeX.
+#' 
+#' For more information about the tsvg package you might consult the 
+#' [tsvg package documentation](http://htmlpreview.github.io/?https://github.com/mittelmark/DGTcl/blob/master/pandoc-tcl-filter/lib/tsvg/tsvg.html).
+#' 
+#' ## INCLUDE FILES
+#' 
+#' An other utility function is the include function where
+#' e can include other files, for instance some which contain as well Markdown markup. This can be used for instance to include common header and footers in your documents. 
+#' Below an example we create an external file and the include this file afterwards,
+#' we would like to have links to all filters here in the document (chunk options - `{.tcl results="asis"})`.
+#' 
+#' ```{.tcl results="asis"} 
+#' set md \n
+#' foreach html [lsort [glob filter*.html]] {
+#'     append md "* \[[regsub {.html} $html {}]\]($html)\n"
+#' }
+#' append md \n
+#' set out [open links.md w 0600]
+#' puts $out $md
+#' close $out
+#' include links.md
+#' ```
+#' 
+#' This allows in a nice way to create the same links for all documentation files such as these
+#' which are on top of this document visible.
+#' 
+#' ## INLINE CODE
+#' 
+#' Short statements can be as well directy embedded into the main text. Here an example:
+#' 
+#' ```
+#' The current time of document creation is : `tcl clock format [clock seconds]`.
+#' ```
+#' 
+#' Here the output:
+#' 
+#' The current time of document creation is : `tcl clock format [clock seconds]`.
+#' 
+#' ## TODO:
+#' 
+#' - more flexible list2mdtab function
+#' - res bug - fixed (vars are now in namespace filter)
+#' 
+#' ```{.tcl}
+#' set vars [lsort [info vars ::*]]
+#' set vars 
+#' ```
+#' 
+#' ## SEE ALSO
+#' 
+#' * [Readme](../Readme.html) - more background
+#' * [pandoc-tcl-filter](../pandoc-tcl-filter.html) - the main filter engine
+#' * [filter-pipe](filter-pipe.html) - Embedding Python, R and Octave code in a similar way like Tcl in the Tcl filter
+#' 
+#' ## AUTHOR
+#' 
+#' * Dr. Detlef Groth, Schwielowsee, Germany
+#' 
+#' ## LICENSE
+#' 
+#' MIT - License
+#' 
+
+mdi eval {
+    namespace eval filter { 
+        variable res 
+        variable chunk
+        set res ""
+        set chunk 0
+    }
+    rename puts puts.orig
+    package provide pandoc 0.7.0
+    proc puts {args} {
+        if {[lindex $args 0] eq "stdout"} {
+            set args [lrange $args 1 end]
+        }
+        if {[regexp {^file} [lindex $args 0]]} {
+            puts.orig [lindex $args 0] {*}[lrange $args 1 end]
+        } else {
+            if {[lindex $args 0] eq "-nonewline"} {
+                append ::filter::res "[lindex $args 1]"
+            } else {
+                append ::filter::res "[lindex $args 0]\n"
+            }
+            return ""
+        }
+    }
+    proc list2mdtab {header values} {
+        set ncol [llength $header]
+        set nval [llength $values]
+        if {[llength [lindex $values 0]] > 1 && [llength [lindex $values 0]] != [llength $header]} {
+            error "Error: list2table - number of values if first row is not a multiple of columns!"
+        } elseif {[expr {int(fmod($nval,$ncol))}] != 0} {
+            error "Error: list2table - number of values is not a multiple of columns!"
+        }
+        set res "|" 
+        foreach h $header {
+            append res " $h |"
+        }   
+        append res "\n|"
+        foreach h $header {
+            append res " ---- |"
+        }
+        append res "\n"
+        set c 0
+        foreach val $values {
+            if {[llength $val] > 1} {    
+                # nested list
+                append res "| "
+                foreach v $val {
+                    append res " $v |"
+                }
+                append res "\n"
+            } else {
+                if {[expr {int(fmod($c,$ncol))}] == 0} {
+                    append res "| " 
+                }    
+                append res " $val |"
+                incr c
+                if {[expr {int(fmod($c,$ncol))}] == 0} {
+                    append res "\n" 
+                }    
+            }
+        }
+        return $res
+    }
+    proc include {filename} {
+        if [catch {open $filename r} infh] {
+            return "Cannot open $filename"
+        } else {
+            set res ""
+            while {[gets $infh line] >= 0} {
+                append res "$line\n"
+            }
+            close $infh
+            return $res
+        }
+    }
+}
+proc filter-tcl {cont a} {
+    set ret ""
+    set b [dict create fig false width 400 height 400 include true \
+            label null]
+    set a [dict merge $b $a]
+    if {[dict get $a eval]} {
+        mdi eval "set ::filter::res {}; incr ::filter::chunk"
+        if {[catch {
+             set eres [mdi eval $cont]
+             set eres "[mdi eval {set ::filter::res}]$eres"
+        }]} {
+             set eres "Error: [regsub {\n +invoked.+} $::errorInfo {}]"
+        }
+        if {[dict get $a fig]} {
+            # figure command there?
+            if {[mdi eval {info command figure}] eq "figure"} {
+                if {[dict get $a label] eq "null"} {
+                    set lab fig-[mdi eval { set ::filter::chunk }]
+                } else {
+                    set lab [dict get $a label]
+                }
+                set filename [mdi eval "figure $lab [dict get $a width] [dict get $a height]"]
+                set eres ""
+            }
+        }
+        set img ""
+        if {[dict get $a results] eq "show" && $eres ne ""} {
+            set eres [regsub {\{(.+)\}} $eres "\\1"]
+            #rl_json::json set cblock c 0 1 [rl_json::json array [list string tclout]]
+            #rl_json::json set cblock c 1 [rl_json::json string [regsub {\{(.+)\}} $eres "\\1"]]
+            #set ret ",[::rl_json::json extract $cblock]"
+        } elseif {[dict get $a results] eq "asis" && $eres ne ""} { 
+            set eres $eres
+        } else {
+            set eres ""
+        }
+    } else {
+        set eres ""
+        set img ""
+    }
+    return [list "$eres" $img]
+}
+
