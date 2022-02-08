d03a46a89c3ec962a8fdfffd8949aca6592b400a
Detlef Groth <dgroth@uni-potsdam.de>
2022-02-08 07:03:12 +0100

    link updates

--- pandoc-tcl-filter/filter/filter-eqn.tcl
+++ pandoc-tcl-filter/filter/filter-eqn.tcl
@@ -11,6 +11,14 @@
 # the script pandoc-tcl-filter.tcl 
 # must be in the in the parent directory of the filter directory
 #' 
+#' ------
+#' 
+#' ```{.tcl results="asis" echo=false}
+#' include header.md
+#' ```
+#' 
+#' ------
+#'
 #' ## Name
 #' 
 #' _filter-eqn.tcl_ - Filter which can be used to render eqn data within a Pandoc processed
